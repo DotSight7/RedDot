b71e903c98df84101aa412cd960e2d7630fcd083
yocto <dbwogk7563@gmail.com>
2020-09-04 12:04:42 +0900

    Update Novus_Poc_install.sh

--- Novus_Poc_install.sh
+++ Novus_Poc_install.sh
@@ -52,7 +52,7 @@ echo -e "
 
 		"Downlod SHIELD@HOME")
 		echo "Downlod SHIELD@HOME >>> /Home"
-		wget --no-check-certificate 'wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1rWmTY9GnmF3Ap0OiKJytbYM0F97HKXDC' -O shieldathome.tar.gz'
+		wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1rWmTY9GnmF3Ap0OiKJytbYM0F97HKXDC' -O shieldathome.tar.gz
 		;;
 
 
